c30ca6392849dab82482de0eccc47c0243baa9ea
paldepind <simonfv@gmail.com>
2015-05-26 09:20:48 +0200

    Update 2015-05-26T07:20:47.691Z

---
+++ js/main.js
@@ -0,0 +1,600 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var benchmark = require('vdom-benchmark-base');
+var snabbdom = require('snabbdom/snabbdom');
+var patch = snabbdom.init([]);
+var h = require('snabbdom/h');
+
+var NAME = 'snabbdom';
+var VERSION = '0.1.0';
+
+function convertToVnodes(nodes) {
+  var n, i, children = [];
+  for (i = 0; i < nodes.length; ++i) {
+    n = nodes[i];
+    if (n.children !== null) {
+      children.push(h('div', {key: n.key}, convertToVnodes(n.children)));
+    } else {
+      children.push(h('span', {key: n.key}, n.key));
+    }
+  }
+  return children;
+}
+
+function BenchmarkImpl(container, a, b) {
+  this.container = container;
+  this.a = a;
+  this.b = b;
+  this.vnode = null;
+}
+
+BenchmarkImpl.prototype.setUp = function() {
+};
+
+BenchmarkImpl.prototype.tearDown = function() {
+  patch(this.vnode, h('div'));
+};
+
+BenchmarkImpl.prototype.render = function() {
+  var elm = document.createElement('div');
+  this.vnode = patch(elm, h('div', convertToVnodes(this.a)));
+  this.container.appendChild(elm);
+};
+
+BenchmarkImpl.prototype.update = function() {
+  this.vnode = patch(this.vnode, h('div', convertToVnodes(this.b)));
+};
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  benchmark(NAME, VERSION, BenchmarkImpl);
+}, false);
+
+},{"snabbdom/h":2,"snabbdom/snabbdom":4,"vdom-benchmark-base":8}],2:[function(require,module,exports){
+var VNode = require('./vnode');
+var is = require('./is');
+
+module.exports = function h(sel, b, c) {
+  var data = {}, children, text, i;
+  if (arguments.length === 3) {
+    data = b;
+    if (is.array(c)) { children = c; }
+    else if (is.primitive(c)) { text = c; }
+  } else if (arguments.length === 2) {
+    if (is.array(b)) { children = b; }
+    else if (is.primitive(b)) { text = b; }
+    else { data = b; }
+  }
+  if (is.array(children)) {
+    for (i = 0; i < children.length; ++i) {
+      if (is.primitive(children[i])) children[i] = VNode(undefined, undefined, undefined, children[i]);
+    }
+  }
+  return VNode(sel, data, children, text, undefined);
+};
+
+},{"./is":3,"./vnode":5}],3:[function(require,module,exports){
+module.exports = {
+  array: Array.isArray,
+  primitive: function(s) { return typeof s === 'string' || typeof s === 'number'; },
+};
+
+},{}],4:[function(require,module,exports){
+// jshint newcap: false
+'use strict';
+
+var VNode = require('./vnode');
+var is = require('./is');
+
+function isUndef(s) { return s === undefined; }
+
+function emptyNodeAt(elm) {
+  return VNode(elm.tagName, {}, [], undefined, elm);
+}
+
+var emptyNode = VNode('', {}, [], undefined, undefined);
+
+var insertedVnodeQueue;
+
+function sameVnode(vnode1, vnode2) {
+  return vnode1.key === vnode2.key && vnode1.sel === vnode2.sel;
+}
+
+function createKeyToOldIdx(children, beginIdx, endIdx) {
+  var i, map = {}, key;
+  for (i = beginIdx; i <= endIdx; ++i) {
+    key = children[i].key;
+    if (!isUndef(key)) map[key] = i;
+  }
+  return map;
+}
+
+function createRmCb(parentElm, childElm, listeners) {
+  return function() {
+    if (--listeners === 0) parentElm.removeChild(childElm);
+  };
+}
+
+var hooks = ['create', 'update', 'remove', 'destroy', 'pre', 'post'];
+
+function init(modules) {
+  var i, j, cbs = {};
+  for (i = 0; i < hooks.length; ++i) {
+    cbs[hooks[i]] = [];
+    for (j = 0; j < modules.length; ++j) {
+      if (modules[j][hooks[i]] !== undefined) cbs[hooks[i]].push(modules[j][hooks[i]]);
+    }
+  }
+
+  function createElm(vnode) {
+    var i;
+    if (!isUndef(i = vnode.data) && !isUndef(i = i.hook) && !isUndef(i = i.init)) {
+      i(vnode);
+    }
+    if (!isUndef(i = vnode.data) && !isUndef(i = i.vnode)) vnode = i;
+    var elm, children = vnode.children, sel = vnode.sel;
+    if (!isUndef(sel)) {
+      // Parse selector
+      var hashIdx = sel.indexOf('#');
+      var dotIdx = sel.indexOf('.', hashIdx);
+      var hash = hashIdx > 0 ? hashIdx : sel.length;
+      var dot = dotIdx > 0 ? dotIdx : sel.length;
+      var tag = hashIdx !== -1 || dotIdx !== -1 ? sel.slice(0, Math.min(hash, dot)) : sel;
+      elm = vnode.elm = document.createElement(tag);
+      if (hash < dot) elm.id = sel.slice(hash + 1, dot);
+      if (dotIdx > 0) elm.className = sel.slice(dot+1).replace(/\./g, ' ');
+      if (is.array(children)) {
+        for (i = 0; i < children.length; ++i) {
+          elm.appendChild(createElm(children[i]));
+        }
+      } else if (is.primitive(vnode.text)) {
+        elm.appendChild(document.createTextNode(vnode.text));
+      }
+      for (i = 0; i < cbs.create.length; ++i) cbs.create[i](emptyNode, vnode);
+      i = vnode.data.hook; // Reuse variable
+      if (!isUndef(i)) {
+        if (i.create) i.create(vnode);
+        if (i.insert) insertedVnodeQueue.push(vnode);
+      }
+    } else {
+      elm = vnode.elm = document.createTextNode(vnode.text);
+    }
+    return elm;
+  }
+
+  function addVnodes(parentElm, before, vnodes, startIdx, endIdx) {
+    if (isUndef(before)) {
+      for (; startIdx <= endIdx; ++startIdx) {
+        parentElm.appendChild(createElm(vnodes[startIdx]));
+      }
+    } else {
+      var elm = before.elm;
+      for (; startIdx <= endIdx; ++startIdx) {
+        parentElm.insertBefore(createElm(vnodes[startIdx]), elm);
+      }
+    }
+  }
+
+  function invokeDestroyHook(vnode) {
+    var i = vnode.data.hook, j;
+    if (!isUndef(i) && !isUndef(j = i.destroy)) j(vnode);
+    for (i = 0; i < cbs.destroy.length; ++i) cbs.destroy[i](vnode);
+    if (!isUndef(vnode.children)) {
+      for (j = 0; j < vnode.children.length; ++j) {
+        invokeDestroyHook(vnode.children[j]);
+      }
+    }
+  }
+
+  function removeVnodes(parentElm, vnodes, startIdx, endIdx) {
+    for (; startIdx <= endIdx; ++startIdx) {
+      var i, listeners, rm, ch = vnodes[startIdx];
+      if (!isUndef(ch)) {
+        listeners = cbs.remove.length + 1;
+        rm = createRmCb(parentElm, ch.elm, listeners);
+        for (i = 0; i < cbs.remove.length; ++i) cbs.remove[i](ch, rm);
+        invokeDestroyHook(ch);
+        if (ch.data.hook && ch.data.hook.remove) {
+          ch.data.hook.remove(ch, rm);
+        } else {
+          rm();
+        }
+      }
+    }
+  }
+
+  function updateChildren(parentElm, oldCh, newCh) {
+    var oldStartIdx = 0, newStartIdx = 0;
+    var oldEndIdx = oldCh.length - 1;
+    var oldStartVnode = oldCh[0];
+    var oldEndVnode = oldCh[oldEndIdx];
+    var newEndIdx = newCh.length - 1;
+    var newStartVnode = newCh[0];
+    var newEndVnode = newCh[newEndIdx];
+    var oldKeyToIdx, idxInOld, elmToMove;
+
+    while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+      if (isUndef(oldStartVnode)) {
+        oldStartVnode = oldCh[++oldStartIdx]; // Vnode has been moved left
+      } else if (isUndef(oldEndVnode)) {
+        oldEndVnode = oldCh[--oldEndIdx];
+      } else if (sameVnode(oldStartVnode, newStartVnode)) {
+        patchVnode(oldStartVnode, newStartVnode);
+        oldStartVnode = oldCh[++oldStartIdx];
+        newStartVnode = newCh[++newStartIdx];
+      } else if (sameVnode(oldEndVnode, newEndVnode)) {
+        patchVnode(oldEndVnode, newEndVnode);
+        oldEndVnode = oldCh[--oldEndIdx];
+        newEndVnode = newCh[--newEndIdx];
+      } else if (sameVnode(oldStartVnode, newEndVnode)) { // Vnode moved right
+        patchVnode(oldStartVnode, newEndVnode);
+        parentElm.insertBefore(oldStartVnode.elm, oldEndVnode.elm.nextSibling);
+        oldStartVnode = oldCh[++oldStartIdx];
+        newEndVnode = newCh[--newEndIdx];
+      } else if (sameVnode(oldEndVnode, newStartVnode)) { // Vnode moved left
+        patchVnode(oldEndVnode, newStartVnode);
+        parentElm.insertBefore(oldEndVnode.elm, oldStartVnode.elm);
+        oldEndVnode = oldCh[--oldEndIdx];
+        newStartVnode = newCh[++newStartIdx];
+      } else {
+        if (isUndef(oldKeyToIdx)) oldKeyToIdx = createKeyToOldIdx(oldCh, oldStartIdx, oldEndIdx);
+        idxInOld = oldKeyToIdx[newStartVnode.key];
+        if (isUndef(idxInOld)) { // New element
+          parentElm.insertBefore(createElm(newStartVnode), oldStartVnode.elm);
+          newStartVnode = newCh[++newStartIdx];
+        } else {
+          elmToMove = oldCh[idxInOld];
+          patchVnode(elmToMove, newStartVnode);
+          oldCh[idxInOld] = undefined;
+          parentElm.insertBefore(elmToMove.elm, oldStartVnode.elm);
+          newStartVnode = newCh[++newStartIdx];
+        }
+      }
+    }
+    if (oldStartIdx > oldEndIdx) addVnodes(parentElm, oldStartVnode, newCh, newStartIdx, newEndIdx);
+    else if (newStartIdx > newEndIdx) removeVnodes(parentElm, oldCh, oldStartIdx, oldEndIdx);
+  }
+
+  function patchVnode(oldVnode, vnode) {
+    var i;
+    if (!isUndef(i = vnode.data) && !isUndef(i = i.hook) && !isUndef(i = i.patch)) {
+      i = i(oldVnode, vnode);
+    }
+    if (!isUndef(i = oldVnode.data) && !isUndef(i = i.vnode)) oldVnode = i;
+    if (!isUndef(i = vnode.data) && !isUndef(i = i.vnode)) vnode = i;
+    var elm = vnode.elm = oldVnode.elm, oldCh = oldVnode.children, ch = vnode.children;
+    if (oldVnode === vnode) return;
+    if (!isUndef(vnode.data)) {
+      for (i = 0; i < cbs.update.length; ++i) cbs.update[i](oldVnode, vnode);
+      i = vnode.data.hook;
+      if (!isUndef(i) && !isUndef(i = i.update)) i(vnode);
+    }
+    if (isUndef(vnode.text)) {
+      if (!isUndef(oldCh) && !isUndef(ch)) {
+        if (oldCh !== ch) updateChildren(elm, oldCh, ch);
+      } else if (!isUndef(ch)) {
+        addVnodes(elm, undefined, ch, 0, ch.length - 1);
+      } else if (!isUndef(oldCh)) {
+        removeVnodes(elm, oldCh, 0, oldCh.length - 1);
+      }
+    } else if (oldVnode.text !== vnode.text) {
+      elm.childNodes[0].nodeValue = vnode.text;
+    }
+    return vnode;
+  }
+
+  return function(oldVnode, vnode) {
+    var i;
+    insertedVnodeQueue = [];
+    if (oldVnode instanceof Element) {
+      oldVnode = emptyNodeAt(oldVnode);
+    }
+    for (i = 0; i < cbs.pre.length; ++i) cbs.pre[i]();
+    patchVnode(oldVnode, vnode);
+    for (i = 0; i < insertedVnodeQueue.length; ++i) {
+      insertedVnodeQueue[i].data.hook.insert(insertedVnodeQueue[i]);
+    }
+    insertedVnodeQueue = undefined;
+    for (i = 0; i < cbs.post.length; ++i) cbs.post[i]();
+    return vnode;
+  };
+}
+
+module.exports = {init: init};
+
+},{"./is":3,"./vnode":5}],5:[function(require,module,exports){
+module.exports = function(sel, data, children, text, elm) {
+  var key = data === undefined ? undefined : data.key;
+  return {sel: sel, data: data, children: children,
+          text: text, elm: elm, key: key};
+};
+
+},{}],6:[function(require,module,exports){
+'use strict';
+
+var Executor = require('./executor');
+
+function Benchmark() {
+  this.running = false;
+  this.impl = null;
+  this.tests = null;
+  this.reportCallback = null;
+
+  this.container = document.createElement('div');
+
+  this._runButton = document.getElementById('RunButton');
+  this._iterationsElement = document.getElementById('Iterations');
+  this._reportElement = document.createElement('pre');
+
+  document.body.appendChild(this.container);
+  document.body.appendChild(this._reportElement);
+
+  var self = this;
+
+  this._runButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (!self.running) {
+      var iterations = parseInt(self._iterationsElement.value);
+      if (iterations <= 0) {
+        iterations = 10;
+      }
+
+      self.run(iterations);
+    }
+  }, false);
+
+  this.ready(true);
+}
+
+Benchmark.prototype.ready = function(v) {
+  if (v) {
+    this._runButton.disabled = '';
+  } else {
+    this._runButton.disabled = 'true';
+  }
+};
+
+Benchmark.prototype.run = function(iterations) {
+  var self = this;
+  this.running = true;
+  this.ready(false);
+
+  new Executor(self.impl, self.container, self.tests, 1, function() { // warmup
+    new Executor(self.impl, self.container, self.tests, iterations, function(samples) {
+      self._reportElement.textContent = JSON.stringify(samples, null, ' ');
+      self.running = false;
+      self.ready(true);
+      if (self.reportCallback != null) {
+        self.reportCallback(samples);
+      }
+    }).start();
+  }).start();
+};
+
+module.exports = Benchmark;
+
+},{"./executor":7}],7:[function(require,module,exports){
+'use strict';
+
+function Executor(impl, container, tests, iterations, cb, iterCb) {
+  if (iterCb === void 0) iterCb = null;
+
+  this.impl = impl;
+  this.container = container;
+  this.tests = tests;
+  this.iterations = iterations;
+  this.cb = cb;
+  this.iterCb = iterCb;
+
+  this._currentTest = 0;
+  this._currentIter = 0;
+  this._renderSamples = [];
+  this._updateSamples = [];
+  this._result = [];
+
+  this._tasksCount = tests.length * iterations;
+
+  this._iter = this.iter.bind(this);
+}
+
+Executor.prototype.start = function() {
+  this._iter();
+};
+
+Executor.prototype.finished = function() {
+  this.cb(this._result);
+};
+
+Executor.prototype.progress = function() {
+  if (this._currentTest === 0 && this._currentIter === 0) {
+    return 0;
+  }
+
+  var tests = this.tests;
+  return (this._currentTest * tests.length + this._currentIter) / (tests.length * this.iterataions);
+};
+
+Executor.prototype.iter = function() {
+  if (this.iterCb != null) {
+    this.iterCb(this);
+  }
+
+  var tests = this.tests;
+
+  if (this._currentTest < tests.length) {
+    var test = tests[this._currentTest];
+
+    if (this._currentIter < this.iterations) {
+      var e, t;
+      var renderTime, updateTime;
+
+      e = new this.impl(this.container, test.data.a, test.data.b);
+      e.setUp();
+
+      t = window.performance.now();
+      e.render();
+      renderTime = window.performance.now() - t;
+
+      t = window.performance.now();
+      e.update();
+      updateTime = window.performance.now() - t;
+      e.tearDown();
+
+      this._renderSamples.push(renderTime);
+      this._updateSamples.push(updateTime);
+
+      this._currentIter++;
+    } else {
+      this._result.push({
+        name: test.name + ' ' + 'render()',
+        data: this._renderSamples.slice(0)
+      });
+
+      this._result.push({
+        name: test.name + ' ' + 'update()',
+        data: this._updateSamples.slice(0)
+      });
+
+      this._currentTest++;
+
+      this._currentIter = 0;
+      this._renderSamples = [];
+      this._updateSamples = [];
+    }
+
+    setTimeout(this._iter, 0);
+  } else {
+    this.finished();
+  }
+};
+
+module.exports = Executor;
+
+},{}],8:[function(require,module,exports){
+'use strict';
+
+var Benchmark = require('./benchmark');
+var benchmark = new Benchmark();
+
+function initFromScript(scriptUrl, impl) {
+  var e = document.createElement('script');
+  e.src = scriptUrl;
+
+  e.onload = function() {
+    benchmark.tests = window.benchmarkTests();
+    benchmark.ready(true);
+  };
+
+  document.head.appendChild(e);
+}
+
+function initFromParentWindow(parent, name, version, id) {
+  window.addEventListener('message', function(e) {
+    var data = e.data;
+    var type = data.type;
+
+    if (type === 'tests') {
+      benchmark.tests = data.data;
+      benchmark.reportCallback = function(samples) {
+        parent.postMessage({
+          type: 'report',
+          data: {
+            name: name,
+            version: version,
+            samples: samples
+          },
+          id: id
+        }, '*');
+      };
+      benchmark.ready(true);
+
+      parent.postMessage({
+        type: 'ready',
+        data: null,
+        id: id
+      }, '*');
+    } else if (type === 'run') {
+      benchmark.run(data.data.iterations);
+    }
+  }, false);
+
+  parent.postMessage({
+    type: 'init',
+    data: null,
+    id: id
+  }, '*');
+}
+
+function init(name, version, impl) {
+  // Parse Query String.
+  var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+      var p=a[i].split('=', 2);
+      if (p.length == 1) {
+        b[p[0]] = "";
+      } else {
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+      }
+    }
+    return b;
+  })(window.location.search.substr(1).split('&'));
+
+  if (qs['name'] !== void 0) {
+    name = qs['name'];
+  }
+
+  if (qs['version'] !== void 0) {
+    version = qs['version'];
+  }
+
+  var type = qs['type'];
+  var id;
+  if (type === 'iframe') {
+    id = qs['id'];
+    if (id === void 0) id = null;
+    initFromParentWindow(window.parent, name, version, id);
+  } else if (type === 'window') {
+    if (window.opener != null) {
+      id = qs['id'];
+      if (id === void 0) id = null;
+      initFromParentWindow(window.opener, name, version, id);
+    } else {
+      console.log('Failed to initialize: opener window is NULL');
+    }
+  } else {
+    var testsUrl = qs['data']; // url to the script generating test data
+    if (testsUrl !== void 0) {
+      initFromScript(testsUrl);
+    } else {
+      console.log('Failed to initialize: cannot load tests data');
+    }
+  }
+
+  benchmark.impl = impl;
+}
+
+// performance.now() polyfill
+// https://gist.github.com/paulirish/5438650
+// prepare base perf object
+if (typeof window.performance === 'undefined') {
+  window.performance = {};
+}
+if (!window.performance.now){
+  var nowOffset = Date.now();
+  if (performance.timing && performance.timing.navigationStart) {
+    nowOffset = performance.timing.navigationStart;
+  }
+  window.performance.now = function now(){
+    return Date.now() - nowOffset;
+  };
+}
+
+module.exports = init;
+
+},{"./benchmark":6}]},{},[1])
+
+
+//# sourceMappingURL=main.js.map
